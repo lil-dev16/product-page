33757463f450e4530d114af82e70cb1a824fad84
lil-dev16 <olusegunkd2017@gmail.com>
2022-08-17 22:12:40 +0100

    functionality

--- script.js
+++ script.js
@@ -7,3 +7,70 @@ bar.addEventListener('click', ()=>{
     console.log('done!!');
 })
 
+const prevBtn = document.querySelector('.previous');
+const nextBtn = document.querySelector('.next');
+const product  = document.querySelector('.product');
+
+const images = [
+    './images/image-product-1.jpg',
+    './images/image-product-2.jpg',
+    './images/image-product-3.jpg',
+    './images/image-product-4.jpg'
+]
+
+const imgMatch = [
+    {
+        id: 1,
+        sor: './images/image-product-1.jpg'
+    },
+    {
+        id: 2,
+        sor: './images/image-product-1.jpg'
+    },
+    {
+        id: 3,
+        sor: './images/image-product-1.jpg'
+    },
+        {
+        id: 4,
+        sor: './images/image-product-1.jpg'
+    }
+]
+
+let target= 0;
+const showImg = (index) => {
+    product.src = images[index];
+
+}
+
+nextBtn.addEventListener('click', ()=> {
+        target++;
+        if(target >= images.length){
+            target = 0;
+        }
+        showImg(target);
+})
+
+prevBtn.addEventListener('click', ()=> {
+        target--;
+        if(target <= -1 ){
+            target = images.length - 1;
+            console.log(target);
+        }
+        showImg(target);
+})
+
+//thumbnails sec
+const thumbnails = document.querySelectorAll('.thumb-img');
+function thumbAction() {
+    product.src= imgMatch[1].sor;
+    thumbnails.forEach(thumb=> {
+        thumb.classList.remove('select')
+        this.classList.add('select')
+        
+    })
+}
+
+thumbnails.forEach(thumb=>{
+    thumb.addEventListener('click', thumbAction)
+})
